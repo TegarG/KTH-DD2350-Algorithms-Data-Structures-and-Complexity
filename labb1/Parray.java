1bf3c4425018727f9afa2a944ace59c155207d79
mrgn-b <morgan.bjalvenas@gmail.com>
2025-09-09 16:15:27 +0200

    Dynamic height

--- labb1/Parray.java
+++ labb1/Parray.java
@@ -35,35 +35,15 @@ public class Parray{
                     System.out.println("Invalid input!");
             }
         }
-
-
-
-        // System.out.println("hello world!");
-
-        // Parray arr = newarray();
-
-        // arr = set(arr, 0, 420);
-        // arr = set(arr, 1, 42);
-        // arr = set(arr, 13, 100);
-        // System.out.println(get(arr, 15));
-        // arr = set(arr, 15, 69);
-        // System.out.println(get(arr, 15));
-        // System.out.println(get(arr, 0));
-        // arr.unset();
-        // System.out.println(get(arr, 15));
-        // System.out.println(get(arr, 13));
-        // arr.unset();
-        // System.out.println(get(arr, 13));
-
     }
 
     private Node root;
-    private final static int height = 31;
-    private int index = 0;
-    private static Stack<Node> vStack = new Stack<Node>();;
+    private int height;
+    private static Stack<ArrVersion> vStack = new Stack<ArrVersion>();;
 
-    public Parray(Node r){
+    public Parray(Node r, int h){
         this.root = r;
+        this.height = h;
     }
 
     public Parray(int h){
@@ -71,13 +51,14 @@ public class Parray{
     }
 
     public static Parray newarray(){
-        vStack.push(null);
-        return new Parray(null);
+        Node newRoot = createEmptyArray(4);
+        vStack.push(new ArrVersion(newRoot, 4));
+        return new Parray(newRoot, 4);
     }
 
-    public Node createEmptyArray(int h){
+    public static Node createEmptyArray(int h){
         // Base case: if h is zero then leaf node
-        if (h == 0) return new Node(index++, null, null);
+        if (h == 0) return new Node(null, null, null);
 
         // Recursive case: create children nodes
         Node left = createEmptyArray(h-1);
@@ -87,53 +68,59 @@ public class Parray{
         return new Node(h, left, right);
     }
 
-
+    /// Sätt a[i] till value och returnera den nya versionen av arrayen. i är ett ickenegativt heltal och value är ett heltal. Såväl i som value är 32-bitstal, dvs högst 231 − 1.
     public static Parray set(Parray a, int i, Integer value){
-        // Sätt a[i] till value och returnera den nya versionen av arrayen. i är ett ickenegativt heltal och value är ett heltal. Såväl i som value är 32-bitstal, dvs högst 231 − 1.
+
+        // Dynamically increase binary tree by creating new root and pointing old root as left child node
+        while (i > ((1 << a.height) - 1) && a.height < 32){
+            Node newRoot = new Node(null, a.root, null);
+            a.height++;
+            a.root = newRoot;
+        }
 
         // Check if index within range, i >2^31 - 1 or i is negative
-        if ((i > ((1 << height) - 1) || i < 0)){
+        if ((i > ((1 << 31) - 1) || i < 0)){
             return null;
         }
 
+        // If user inputs null value, return null
         if (value == null){
             return null;
         }
 
+        // Create new tree root and push to version stack, return array
         Node newRoot = set(a.root, i, value, a.height);
-        vStack.push(newRoot);
-        return new Parray(newRoot);
-        // Traversera trädet ->
-        // Om höjden är 0, då är vi vid ett löv och lägger in värde
-        // Om höjden är inte > 0, skapa ny nod och gå till antingen höger eller vänster
-        // Noden som inte ersätts skapar vi en pekare till från den nya noden
+        vStack.push(new ArrVersion(newRoot, a.height));
+        return new Parray(newRoot, a.height);
     }
 
     public static Node set(Node current, int i, Integer value, int h){
-        // Set leaf to value
+        // If height is 0 -> leaf, set leaf to value
         if (h == 0){
             return new Node(value, null, null);
         }
+
+        // If there is no node -> create placeholder
         if (current == null) current = new Node(null, null, null);
 
         // Check direction on each level
         int direction = i & (1 << (h-1));
 
-        if (direction == 0){
+        if (direction == 0){ // Left
+            // Create left node recursively, and return newly created current node preserving right child node
             Node left = set(current.left, i, value, h-1);
-
             return new Node(null, left, current.right);
         }else{
+            // Create right node recursively, and return newly created current node preserving left child node
             Node right = set(current.right, i, value, h-1);
             return new Node(null, current.left, right);
         }
     }
 
+    /// Returnera värdet a[i]. Om a[i] inte har tilldelats ett värde tidigare returneras 0.
     public static int get(Parray a, int i){
-        // Returnera värdet a[i]. Om a[i] inte har tilldelats ett värde tidigare returneras 0.
-
         // Check if index within range, i >2^31 - 1 or i is negative
-        if ((i > ((1 << height) - 1) || i < 0)){
+        if ((i > ((1 << a.height) - 1) || i < 0)){
             return 0;
         }
 
@@ -143,8 +130,15 @@ public class Parray{
     public static int get(Node n, int i, int h){
         // If requested node does not exist return 0
         if (n == null) return 0;
-        // If leaf return value
-        if (h == 0) return n.value;
+
+        // If leaf -> return value
+        if (h == 0){
+            if(n.value == null){// No value
+                return 0; 
+            } else{
+                return n.value;
+            }
+        }
 
         // Check direction on each level
         int direction = i & (1 << (h-1));
@@ -158,11 +152,13 @@ public class Parray{
     }
 
     public void unset(){
-        // Unset function should retrieve the previous versions tree root
-        // The roots should be stored in a LIFO data structure, either stack or linked list. I believe a linked list will be easier to implement dynamically.
-        // The set function should push the new version to the data structure.
-        vStack.pop();
-        this.root = vStack.peek();
+        // Always preserve first version
+        if (vStack.size() == 1) return;
+
+        vStack.pop();// Pop current version
+        ArrVersion previous = vStack.peek(); // Retrieve previous version
+        this.root = previous.root;
+        this.height = previous.height;
     }
 
     public static class Node{
@@ -174,6 +170,16 @@ public class Parray{
             this.value = value;
             this.left = left;
             this.right = right;
+        }
+    }
+
+    public static class ArrVersion{
+        public Node root;
+        public int height;
+
+        public ArrVersion(Node r, int h){
+            this.root = r;
+            this.height = h;
         }
     }
 
